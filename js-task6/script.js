44aca277bcadc6e2f326423fbbe30245b5951c98
Nabiyatulmisriya <92315263+Nabiyatulmisriya@users.noreply.github.com>
2021-10-20 22:13:50 +0530

    Add files via upload

---
+++ js-task6/script.js
@@ -0,0 +1,19 @@
+var para1 = document.getElementsByClassName("xyz");
+console.log(para1);
+//classname
+
+var para2 = document.getElementsByTagName("p");
+console.log(para2);
+//tagname
+
+var para3 = document.querySelector(".xyz");
+console.log(para3);
+//queryselector
+
+var para4 = document.querySelectorAll(".xyz");
+console.log(para4);
+//queryselectorall
+
+var tag = document.getElementById("abc");
+console.log(tag);
+//by id
